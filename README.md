77e1eccf858657f0895bc19b4cdea26fb0d02fdd
NeXT <kingideayou@qq.com>
2015-07-05 16:09:08 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Android PagerSlidingTabStrip Eazy Tab
+根据 [PagerSlidingTabStrip](https://github.com/astuetz/PagerSlidingTabStrip) 项目修改而来，方便给顶部选择的 Tab 添加 BadgeView 显示红点
+
+###[博客地址](http://kingideayou.github.io/2015/07/02/tab_header_badge/)
+
+### 效果图
+<img src="https://raw.githubusercontent.com/kingideayou/PagerSlidingTabStripWithBadgeTab/master/pic/demo_img1.png" width = "380" height = "620" alt="效果图 TextView" align=center />
+
+<img src="https://raw.githubusercontent.com/kingideayou/PagerSlidingTabStripWithBadgeTab/master/pic/demo_img2.png" width = "380" height = "620" alt="效果图 ImageView" align=center />
+
+
+# License
+
+    Copyright 2013 Andreas Stuetz
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ sample/src/com/astuetz/viewpager/extensions/sample/QuickContactFragment.java
@@ -0,0 +1,147 @@
+package com.astuetz.viewpager.extensions.sample;
+
+import android.content.Context;
+import android.graphics.Point;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.TypedValue;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.TextView;
+
+import com.next.PagerSlidingTabStrip;
+import com.next.PagerSlidingTabStrip.IconTabProvider;
+import com.readystatesoftware.viewbadger.BadgeView;
+
+public class QuickContactFragment extends DialogFragment {
+
+	private PagerSlidingTabStrip tabs;
+	private ViewPager pager;
+	private ContactPagerAdapter adapter;
+
+	public static QuickContactFragment newInstance() {
+		QuickContactFragment f = new QuickContactFragment();
+		return f;
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+		if (getDialog() != null) {
+			getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+			getDialog().getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+		}
+
+		View root = inflater.inflate(R.layout.fragment_quick_contact, container, false);
+
+		tabs = (PagerSlidingTabStrip) root.findViewById(R.id.tabs);
+		pager = (ViewPager) root.findViewById(R.id.pager);
+		adapter = new ContactPagerAdapter();
+
+		pager.setAdapter(adapter);
+
+		tabs.setViewPager(pager);
+
+		View targetView = tabs.getTabsContainer().getChildAt(2);
+		BadgeView badge = new BadgeView(getActivity(), targetView);
+		badge.setWidth(dp2px(getActivity(), 8));
+		badge.setHeight(dp2px(getActivity(), 8));
+		badge.setBadgePosition(BadgeView.POSITION_TOP_RIGHT);
+		badge.setBadgeMargin(16, 16);
+		badge.setText(" ");
+		badge.show();
+
+		return root;
+	}
+
+	public static int dp2px(Context context, float dp)
+	{
+		int px = Math.round(TypedValue.applyDimension(
+				TypedValue.COMPLEX_UNIT_DIP, dp, context.getResources()
+						.getDisplayMetrics()));
+		return px;
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public void onStart() {
+		super.onStart();
+
+		// change dialog width
+		if (getDialog() != null) {
+
+			int fullWidth = getDialog().getWindow().getAttributes().width;
+
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+				Display display = getActivity().getWindowManager().getDefaultDisplay();
+				Point size = new Point();
+				display.getSize(size);
+				fullWidth = size.x;
+			} else {
+				Display display = getActivity().getWindowManager().getDefaultDisplay();
+				fullWidth = display.getWidth();
+			}
+
+			final int padding = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 24, getResources()
+					.getDisplayMetrics());
+
+			int w = fullWidth - padding;
+			int h = getDialog().getWindow().getAttributes().height;
+
+			getDialog().getWindow().setLayout(w, h);
+		}
+	}
+
+	public class ContactPagerAdapter extends PagerAdapter implements IconTabProvider {
+
+		private final int[] ICONS = { R.drawable.ic_launcher_gplus, R.drawable.ic_launcher_gmail,
+				R.drawable.ic_launcher_gmaps, R.drawable.ic_launcher_chrome };
+
+		public ContactPagerAdapter() {
+			super();
+		}
+
+		@Override
+		public int getCount() {
+			return ICONS.length;
+		}
+
+		@Override
+		public int getPageIconResId(int position) {
+			return ICONS[position];
+		}
+
+		@Override
+		public Object instantiateItem(ViewGroup container, int position) {
+			// looks a little bit messy here
+			TextView v = new TextView(getActivity());
+			v.setBackgroundResource(R.color.background_window);
+			v.setText("PAGE " + (position + 1));
+			final int padding = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 16, getResources()
+					.getDisplayMetrics());
+			v.setPadding(padding, padding, padding, padding);
+			v.setGravity(Gravity.CENTER);
+			container.addView(v, 0);
+			return v;
+		}
+
+		@Override
+		public void destroyItem(ViewGroup container, int position, Object view) {
+			container.removeView((View) view);
+		}
+
+		@Override
+		public boolean isViewFromObject(View v, Object o) {
+			return v == ((View) o);
+		}
+
+	}
+
+}
